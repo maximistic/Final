9cddb9f949c9aee4f1135ed677324ee7d2d30891
maximistic <110153672+maximistic@users.noreply.github.com>
2025-01-19 11:19:53 +0530

    map elites update

--- Map Elites/main.py
+++ Map Elites/main.py
@@ -1,15 +1,28 @@
-import os
+from map_elites import MapElites
+from ppo.args import add_ppo_args
 import argparse
-import yaml
-from modules.evolution.evolution import evolve
-from modules.utils.helpers import create_dirs
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", type=str, default="config/config.yaml", help="Path to the config file.")
+    parser.add_argument("--exp-name", type=str, default="map_elites_test")
+    parser.add_argument("--env-name", type=str, default="Walker-v0")
+    parser.add_argument("--num-iterations", type=int, default=1000)
+    parser.add_argument("--population-size", type=int, default=50)
+    parser.add_argument("--mutation-rate", type=float, default=0.2)
+    parser.add_argument("--crossover-rate", type=float, default=0.1)
+    add_ppo_args(parser)
     args = parser.parse_args()
-    with open(args.config, "r") as f:
-        config = yaml.safe_load(f)
-    save_dir = config["save_dir"]
-    create_dirs([save_dir])
-    evolve(config)
+
+    feature_dimensions = [(0, 25, 10), (0, 25, 10)]
+    map_elites = MapElites(
+        feature_dimensions=feature_dimensions,
+        population_size=args.population_size,
+        mutation_rate=args.mutation_rate,
+        crossover_rate=args.crossover_rate,
+        num_iterations=args.num_iterations,
+        args=args,
+        env_name=args.env_name,
+    )
+    grid = map_elites.run()
+
+    print("MAP-Elites Completed!")
