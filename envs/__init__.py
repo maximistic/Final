84d7f1ac7886327a02915ebe6cf409eec16c65e9
maximistic <110153672+maximistic@users.noreply.github.com>
2025-01-24 12:13:28 +0530

    update

--- envs/__init__.py
+++
@@ -1,8 +0,0 @@
-from Final.envs.simple_env import SimpleWalkerEnvClass
-from gymnasium.envs.registration import register
-
-register(
-    id="SimpleWalkerEnv-v0",
-    entry_point="Final.envs.simple_env:SimpleWalkerEnvClass",
-    max_episode_steps=500
-)
